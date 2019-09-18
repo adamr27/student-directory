b86df5fb690d82b75b6450249d4845a8e6095a20
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-09-18 15:27:20 +0100

    updated directory

--- directory.rb
+++ directory.rb
@@ -30,6 +30,24 @@ puts "Overall, we have #{names.count} great student"
 end
 #nothing happebs until we call the methods
 
+def input_students
+  puts "Please enter the name of the student"
+  puts "To finish, just hit return twice"
+  #create an emtpy array
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "now we have #{students.count} stundents"
+    name = gets.chomp
+  end
+  students
+  # return the array of stundents
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
